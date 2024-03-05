be14cac37289d70b4f13d1146fa00fec2cbb423d
Jasmine Sewell <146713369+sc23js2@users.noreply.github.com>
2024-03-05 11:25:10 +0000

    Add files via upload

---
+++ cwk1-files/sqlcwk.sql
@@ -0,0 +1,71 @@
+/*
+@author
+
+This is an sql file to put your queries for SQL coursework. 
+You can write your comment in sqlite with -- or /* * /
+
+To read the sql and execute it in the sqlite, simply
+type .read sqlcwk.sql on the terminal after sqlite3 musicstore.db.
+*/
+
+/* =====================================================
+   WARNNIG: DO NOT REMOVE THE DROP VIEW
+   Dropping existing views if exists
+   =====================================================
+*/
+DROP VIEW IF EXISTS vNoCustomerEmployee; 
+DROP VIEW IF EXISTS v10MostSoldMusicGenres; 
+DROP VIEW IF EXISTS vTopAlbumEachGenre; 
+DROP VIEW IF EXISTS v20TopSellingArtists; 
+DROP VIEW IF EXISTS vTopCustomerEachGenre; 
+
+/*
+============================================================================
+Task 1: Complete the query for vNoCustomerEmployee.
+DO NOT REMOVE THE STATEMENT "CREATE VIEW vNoCustomerEmployee AS"
+============================================================================
+*/
+CREATE VIEW vNoCustomerEmployee AS
+--Remove this line and complete your query for Task 1 here
+
+
+/*
+============================================================================
+Task 2: Complete the query for v10MostSoldMusicGenres
+DO NOT REMOVE THE STATEMENT "CREATE VIEW v10MostSoldMusicGenres AS"
+============================================================================
+*/
+CREATE VIEW v10MostSoldMusicGenres AS
+--Remove this line and complete your query for Task 2 here
+
+
+/*
+============================================================================
+Task 3: Complete the query for vTopAlbumEachGenre
+DO NOT REMOVE THE STATEMENT "CREATE VIEW vTopAlbumEachGenre AS"
+============================================================================
+*/
+CREATE VIEW vTopAlbumEachGenre AS
+--Remove this line and complete your query for Task 3 here
+
+
+/*
+============================================================================
+Task 4: Complete the query for v20TopSellingArtists
+DO NOT REMOVE THE STATEMENT "CREATE VIEW v20TopSellingArtists AS"
+============================================================================
+*/
+
+CREATE VIEW v20TopSellingArtists AS
+--Remove this line and complete your query for Task 4 here
+
+
+/*
+============================================================================
+Task 5: Complete the query for vTopCustomerEachGenre
+DO NOT REMOVE THE STATEMENT "CREATE VIEW vTopCustomerEachGenre AS" 
+============================================================================
+*/
+CREATE VIEW vTopCustomerEachGenre AS
+--Remove this line and complete your query for Task 5 here
+
